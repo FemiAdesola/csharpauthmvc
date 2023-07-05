9579be8eebaad6dd2db440a65fb14e3db2503151
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-07-05 12:04:10 +0300

    creates post register

--- Controllers/AppUserController.cs
+++ Controllers/AppUserController.cs
@@ -1,10 +1,26 @@
+using Csharpauth.Database;
 using Csharpauth.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Csharpauth.Controllers
 {
     public class AppUserController : BaseApiController
     {
+        private readonly UserManager<IdentityUser> _userManager;
+         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly AppDbContext _context;
+         public AppUserController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            AppDbContext context
+
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -18,6 +34,40 @@ namespace Csharpauth.Controllers
 
             Register register = new Register();
             return View(register);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Register(Register model)
+        {
+
+            if(ModelState.IsValid)
+            {
+                var user = new AppUser { 
+                    UserName = model.Email, 
+                    Email = model.Email, 
+                    Name = model.Name 
+                    };
+                
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
