f92ac77fe9c492f868ea81d6b75ccc0d1d448bee
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-07-12 09:12:51 +0300

    fixed bug for roles register

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -116,6 +116,18 @@ namespace Csharpauth.Controllers
                 }
                 AddErrors(result);
             }
+             List<SelectListItem> listItems = new List<SelectListItem>();
+            listItems.Add(new SelectListItem()
+            {
+                Value = "Admin",
+                Text = "Admin"
+            });
+            listItems.Add(new SelectListItem()
+            {
+                Value = "User",
+                Text = "User"
+            });
+            model.RoleList = listItems;
 
             return View(model);
         }
