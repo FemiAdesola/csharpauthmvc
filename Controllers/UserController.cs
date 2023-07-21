5ac16f90c9747b3fb049c317e6dd1cf3c0c35f84
Femi Adesola Oyinloye <75606768+FemiAdesola@users.noreply.github.com>
2023-07-21 20:33:27 +0300

    added how to edit user roles to usercontroller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -63,5 +63,43 @@ namespace Csharpauth.Controllers
             });
             return View(objFromDb);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(AppUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var objFromDb = _context.AppUsers.FirstOrDefault(u => u.Id == user.Id);
+                if (objFromDb == null)
+                {
+                    return NotFound();
+                }
+                // about the previous role
+                var userRole = _context.UserRoles.FirstOrDefault(u => u.UserId == objFromDb.Id);
+                if (userRole != null)
+                {
+                    var previousRoleName = _context.Roles.Where(u => u.Id == userRole.RoleId).Select(e => e.Name).FirstOrDefault();
+                    
+                    //how to remove previous role
+                    await _userManager.RemoveFromRoleAsync(objFromDb, previousRoleName!);
+                }
+
+                //how to add new role
+                await _userManager.AddToRoleAsync(objFromDb, _context.Roles.FirstOrDefault(u => u.Id == user.RoleId)!.Name!);
+                objFromDb.Name = user.Name;
+                _context.SaveChanges();
+                TempData[StaticToarst.Success] = "User has been edited successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            user.RoleList = _context.Roles.Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id
+            });
+            return View(user);
+        }
     }
 }
